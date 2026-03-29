3e7174963c7d7d018d9058585fc70d744bfbab33
Rodrigo Ferreira <rodrigo.slva.ferreira@gmail.com>
2026-03-29 16:47:58 +0100

    add missing stuff in shop page component

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,5 +1,17 @@
 import { Link, useOutletContext } from 'react-router';
 
+function ProductItem({ name, image, price }) {
+    return (
+        <div className="product flex flex-column">
+            <img src={image} alt={name} className='product-image'/>
+            <div className="product-info flex flex-column">
+                <span className="product-price">{price} €</span>
+                <span className="product-name">{name}</span>
+            </div>
+        </div>
+    );
+}
+
 export default function Shop() {
     const context = useOutletContext();
 
@@ -9,8 +21,11 @@ export default function Shop() {
                 <h1>Our products</h1>
                 <div id="products-list" className='grid'>
                     {context.products.map(p => {
-                        return <Link key={p.id} to={`/product/${p.id}`}>{p.title}</Link>
-                    })}
+                        return (
+                        <Link key={p.id} to={`/product/${p.id}`}>
+                            <ProductItem name={p.title} image={p.image} price={p.price}></ProductItem>
+                        </Link>
+                    )})}
                 </div>
             </main>
         </>
